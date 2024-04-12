fb00a4ce64483dee5374a83d18239a9ead5b004d
nidhijangid007 <nidhijayalwal007@gmail.com>
2024-04-12 15:47:03 +0530

    basic

---
+++ loop.js
@@ -0,0 +1,20 @@
+// let i,j;
+//         for(i=1;i<4;i++)
+//         {
+//             for(j=1;j<4;j++){
+//                 //console.log(j);
+//                 process.stdout.write('*');
+//             }
+//             console.log();
+//         }
+
+
+let i=8,count;
+if(i%2===0)
+{
+    count++;
+}
+    if(count==2)
+    
+
+    console.log(i);
